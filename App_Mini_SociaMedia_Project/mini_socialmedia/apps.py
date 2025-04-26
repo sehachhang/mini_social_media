ff9c5a4e6929631a971fd55928a1c88a8adcef97
Seha Chhang <124413642+sehachhang@users.noreply.github.com>
2025-04-26 22:12:31 +0700

    Delete App_Mini_SociaMedia_Project directory

--- App_Mini_SociaMedia_Project/mini_socialmedia/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MiniSocialmediaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mini_socialmedia'
--- App_Mini_SociaMedia_Project/mini_socialmedia/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('create-post/', views.create_post, name='create_post'),
-    path('post/<int:post_id>/comment/', views.add_comment, name='add_comment'),
-]
